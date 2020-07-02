1386a8c91f97c6b1c3d2173435f47138374f86ab
shuaibu78 <freezzemx@gmail.com>
2020-07-02 12:15:35 +0100

    completed the project

---
+++ build/precache-manifest.68311d295294345666c35f00eeba4eef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "749f82ca300b4892f5b9bd670f31cd9f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "96ae524f4c7f38eef585",
+    "url": "/static/css/main.13c8a689.chunk.css"
+  },
+  {
+    "revision": "ad6fe01d15d32e169810",
+    "url": "/static/js/2.faaba4d0.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/static/js/2.faaba4d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96ae524f4c7f38eef585",
+    "url": "/static/js/main.9b9508b9.chunk.js"
+  },
+  {
+    "revision": "e173dfe45a061c6e2715",
+    "url": "/static/js/runtime-main.20a4ba51.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/static/media/image.d7265326.png"
+  }
+]);
